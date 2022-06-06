317d7f290c1abfa6383d9029d3502a6aef632348
badeadrian <badeadrian96@gmail.com>
2022-06-06 12:17:12 +0300

    extra import

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -779,4 +779,4 @@ $databases['default']['default'] = array (
   'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
   'driver' => 'mysql',
 );
-$settings['config_sync_directory'] = 'config/sync';
+$settings['config_sync_directory'] = '../config/sync';
